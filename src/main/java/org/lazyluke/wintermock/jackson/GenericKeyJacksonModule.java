2447d30458e9ca64a75c0a690f17a47ce3ff5e1a
Tim Azzopardi <tim.azzopardi@gmail.com>
2017-02-18 19:39:50 +0000

    fix travis.yml to exclude jdk8 for now

--- src/main/java/org/lazyluke/wintermock/jackson/GenericKeyJacksonModule.java
+++ src/main/java/org/lazyluke/wintermock/jackson/GenericKeyJacksonModule.java
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 /**
- * A Jackson module for serializing and deserializing java Map keys that are not Strings
+ * A Jackson module for serializing and deserializing java Map keys that are not Strings.
+ * By default Jackson only serializes Java Map keys using toString. This module serializes the key to Json.
  *
  * @param <T> Java Map key type
  */
